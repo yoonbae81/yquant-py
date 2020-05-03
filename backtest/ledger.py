0f8be105d49113a7a2eb877445a2ca726a0678bd
Yoonbae Cho <y@xcv.kr>
2020-05-03 20:35:16 +0900

    Add mocks

--- backtest/ledger.py
+++ backtest/ledger.py
@@ -20,9 +20,7 @@ class Ledger(Thread):
         self.input: Connection
 
         self._loop: bool = True
-        self._dir: Path = dir
-        self._file: TextIO = self._open_file()
-        logger.debug(f'Generated file: {self._file.name}')
+        self._file: TextIO = self._create_file(dir)
 
         self._handlers: Dict[str, Callable[[Msg], None]] = {
             'CASH': self._handler_cash,
@@ -30,39 +28,34 @@ class Ledger(Thread):
             'QUIT': self._handler_quit,
         }
 
-        logger.debug('Initialized')
-
-    def _open_file(self) -> TextIO:
-        self._dir.mkdir(parents=True, exist_ok=True)
+    @staticmethod
+    def _create_file(dir: Path) -> TextIO:
+        dir.mkdir(parents=True, exist_ok=True)
         filename = f'{datetime.now():%Y%m%d%H%M%S}.jsonl'
+        logger.debug(f'Preparing file: {filename}')
 
-        return self._dir.joinpath(filename).open('wt')
+        return dir.joinpath(filename).open('w')
 
     def run(self) -> None:
-        logger.debug('Starting...')
-
         while self._loop:
             msg = self.input.recv()
             logger.debug(f'Received: {msg}')
             self._handlers[msg.type](msg)
 
-        if self._file and not self._file.closed:
-            logger.info(f'Closing {self._file.name}')
-            self._file.close()
-
     def _handler_cash(self, msg: Msg) -> None:
         if msg.cash < 0:
             raise ArithmeticError('Cash should be greater than zero')
 
-        json.dump({'cash': msg.cash}, self._file)
-        self._file.write('\n')
+        output = json.dumps({'cash': msg.cash})
+        print(output, file=self._file)
 
     def _handler_order(self, msg: Msg) -> None:
-        json.dump(asdict(msg), self._file)
-        self._file.write('\n')
+        output = json.dumps(asdict(msg))
+        print(output, file=self._file)
 
     def _handler_quit(self, _: Msg) -> None:
         self._loop = False
 
-    def __del__(self) -> None:
-        pass
+    def __del__(self):
+        if self._file and not self._file.close():
+            self._file.close()
--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -4,6 +4,7 @@ from contextlib import nullcontext as does_not_raise
 from datetime import datetime
 from multiprocessing.connection import Pipe
 from pathlib import Path
+from unittest import mock
 
 import pytest
 
@@ -16,29 +17,13 @@ def test_init(tmp_path):
     assert sut
 
 
-def test_open_file(tmp_path):
+def test_create_file(tmp_path):
     Sut(tmp_path)
     today = f'{datetime.now():%Y%m%d}'
     assert tmp_path.glob(f'{today}*.json')
 
 
-@pytest.mark.parametrize('input, expected', [
-    ({'cash': 10}, does_not_raise()),
-    ({'cash': 'AAA'}, pytest.raises(TypeError)),
-    ({'cash': -1}, pytest.raises(ArithmeticError)),
-])
-def test_cash(tmp_path, input, expected):
-    sut = Sut(tmp_path)
-    with expected:
-        sut._handler_cash(Msg('CASH', **input))
-    sut._file.close()
-
-    if isinstance(input['cash'], numbers.Number) and input['cash'] > 0:
-        text = Path(sut._file.name).read_text()
-        assert json.loads(text)['cash'] == input['cash']
-
-
-def test_quit(tmp_path):
+def test_handler_quit(tmp_path):
     sut = Sut(tmp_path)
     sut.input, router = Pipe(duplex=False)
 
@@ -47,3 +32,37 @@ def test_quit(tmp_path):
     sut.join()
 
     assert True
+
+
+@pytest.mark.parametrize('input, expected', [
+    ({'cash': 10}, 10),
+])
+def test_handler_cash_ok(tmp_path, input, expected):
+    sut = Sut(tmp_path)
+
+    sut._file.close()
+    text = Path(sut._file.name).read_text()
+    assert json.loads(text)['cash'] == expected
+
+
+@mock.patch('ledger.print')
+def test_handler_cash_ok2(tmp_path, mock_print):
+    sut = Sut(tmp_path)
+
+    mock_print.assert_called_once_with('{"cash": 10}')
+
+
+@pytest.mark.parametrize('input, expected', [
+    ({'cash': 10}, does_not_raise()),
+    ({'cash': -1}, pytest.raises(ArithmeticError)),
+    ({'cash': 'STRING_IS_NOT_ACCEPTABLE'}, pytest.raises(TypeError)),
+])
+def test_handler_cash_error(tmp_path, input, expected):
+    sut = Sut(tmp_path)
+
+    with expected:
+        sut._handler_cash(Msg('CASH', **input))
+
+
+def test_handler_order():
+    pass
