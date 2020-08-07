49ec833c563d148ece9382f9f681b79fc01f3a48
Yoonbae Cho <y@xcv.kr>
2020-08-07 12:45:33 +0900

    Update evaluator.py

--- backtest/evaluator.py
+++ backtest/evaluator.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 logger = logging.getLogger(Path(__file__).stem)
 
+
 def evaluate(tick_files, ledger_file, benchmark_symbol, factors=[]):
     return {
         'profit': 99,
