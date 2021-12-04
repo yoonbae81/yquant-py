42cd34ed41dd51c522eb21a8846fb2287e89b8fe
Yoonbae Cho <y@xcv.kr>
2021-12-04 13:39:27 +0400

    Update

--- demo/ib21.py
+++
@@ -1,3 +0,0 @@
-from yquant.backtest import bootstrap
-
-bootstrap.run()
---
+++ demo/sample.py
@@ -0,0 +1,13 @@
+from yquant.backtest import bootstrap
+
+def calc_strength():
+    pass
+
+
+def calc_quantity():
+    pass
+
+
+
+
+yquant.backtest()
